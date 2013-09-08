136fbdc0a61454f5cb5c593beb8baeee547e13fe
2p-hala <hadly@2p.com.sa>
2013-09-09 01:49:41 +0200

    new images

---
+++ QuraanMindMapping/src/com/hm/mindmap/MindMapActivity.java
@@ -0,0 +1,24 @@
+package com.hm.mindmap;
+
+import android.os.Bundle;
+import android.view.View;
+import c.PopUpDialog;
+
+public class MindMapActivity extends ParentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		setContentView(R.layout.activity_mind_map);
+		super.onCreate(savedInstanceState);
+		findViewById(R.id.help).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				new PopUpDialog(MindMapActivity.this).show(R.string.mindmap_tit,
+						R.string.mindmap_data);
+			}
+		});
+	}
+
+}
